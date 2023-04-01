4a277a6cd2c0069e53a06fc72e64d6c64a489f0e
mateorinland <121818729+mateorinland@users.noreply.github.com>
2023-04-01 09:56:19 -0300

    Added interactivity

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const radioLabels = document.querySelectorAll(".radio-label");
-const submitButton = document.querySelector(".submit-button");
-const ratingState = document.querySelector(".rating-state");
+const radioInputs = document.querySelectorAll('.radio-input');
+const submitButton = document.querySelector('.submit-button');
+const form = document.querySelector('.js-rating-form');
 const thankYouState = document.querySelector(".thank-you-state");
+const ratingState = document.querySelector(".rating-state");
 const feedbackText = document.querySelector(".feedback-text");
 
-//Selecting radio label
-radioLabels.forEach((label) => {
-    label.addEventListener("click", function() {
-        radioLabels.forEach((whenSelected) => {
-            whenSelected.classList.remove("selected");
-        });
-        label.classList.add("selected");
+//Enable submit button
+radioInputs.forEach(input => {
+    input.addEventListener('change', () => {
+        submitButton.disabled = false;
     });
 });
 
-//Rendering selected value
-submitButton.addEventListener("click", function () {
-    console.log("Button click");
-    const selectedLabel = document.querySelector(".selected");
-    let selectedValue = selectedLabel.textContent;
+
+//Render selected value
+function handleSubmit(e) {
+  e.preventDefault();
+  if (form.rating.value) {
     thankYouState.classList.add("show");
     ratingState.classList.add("hide");
-    feedbackText.textContent = `You selected ${selectedValue} out of 5`;
-  });
+    feedbackText.textContent = `You selected ${form.rating.value} out of 5`;
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
